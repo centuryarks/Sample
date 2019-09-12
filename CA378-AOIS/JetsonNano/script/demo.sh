db0e8e4d299489b62dae254be24d41c7e65a14a7
KimiharuNishihata <kimiharu-nishihata@centuryarks.com>
2019-09-12 16:46:39 +0900

    Update VGA mode fps settings.(from 340 to 240)

--- CA378-AOIS/JetsonNano/script/demo.sh
+++ CA378-AOIS/JetsonNano/script/demo.sh
@@ -4,4 +4,4 @@ cd /home/nvidia/demo
 sudo ./bin/DemoGUI -id $ID -w 4032 -h 3040 -fps 20
 #sudo ./bin/DemoGUI -id $ID -w 3840 -h 2160 -fps 30
 #sudo ./bin/DemoGUI -id $ID -w 1920 -h 1080 -fps 120
-#sudo ./bin/DemoGUI -id $ID -w 640 -h 480 -fps 340
+#sudo ./bin/DemoGUI -id $ID -w 640 -h 480 -fps 240
